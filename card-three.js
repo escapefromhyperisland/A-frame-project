1c16ab4ba94b22d8a22efc132a9f65163e7b7a08
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-06-07 21:23:49 +0200

    functionality added to all boxes

--- card-three.js
+++ card-three.js
@@ -1,20 +1,19 @@
-
-AFRAME.registerComponent('card-one', {
+AFRAME.registerComponent('card-three', {
     init: function () {
       var el = this.el;
       el.addEventListener('click', function (evt) {
         console.log("i m here")
       var pos = el.getAttribute('position')
       console.log(pos)
-      if( pos.x <= -4 && pos.x >= -6 && pos.z <=14 && pos.z >=12) {
-        let boxOne=document.getElementById("boxOne");
-        let imageOne=document.getElementById("imageOne");
-        let image=document.getElementById("aImage1");
+      if( pos.x <= -6 && pos.x >= -8 && pos.z <=13 && pos.z >=12) {
+        let boxOne=document.getElementById("boxThree");
+        let imageOne=document.getElementById("imageThree");
+        let image2=document.getElementById("aImage3");
         
         imageOne.classList.add("displayNone");
         console.log(imageOne)
         boxOne.setAttribute('visible','false');
-        image.setAttribute('visible','false');
+        image2.setAttribute('visible','false');
       }
     })
   }
